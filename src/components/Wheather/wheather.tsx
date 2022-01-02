b57b81dcb1b90268bacaefd64f51100db5b85c4f
madisonrubylee <lamantciel@naver.com>
2022-01-02 17:35:03 +0900

    add date

--- src/components/Wheather/wheather.tsx
+++ src/components/Wheather/wheather.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import styles from './wheather.module.css'
+import moment from 'moment'
 
 const DEFAULT_IMAGE = '/wheather/default_wheather.png'
 
 const Wheather = () => {
-
+    
+    const now = new Date()
+    const nowDate = `${moment(now).format(`dddd, D MM YYYY`)}`
     return (
         <>
+            <p className={styles.nowDate} >{ nowDate }</p>
             <img className={styles.img} src={DEFAULT_IMAGE} alt="wheather" />
         </> 
     )
